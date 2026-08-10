2dce164d11c696c9bb07205ccd19abb3bd49a2f1
abdellrhmanmansour9-cpu <abdellrhmanmansour9@gmail.com>
2026-08-10 14:39:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(
-    page_title="BelYarn",
+    page_title="BelYarn Quality Intelligence Platform",
+    page_icon="🧵",
     layout="wide"
 )
 
 st.title("🧵 BelYarn Quality Intelligence Platform")
 
-st.success("Application is running successfully")
+uploaded_file = st.file_uploader(
+    "Upload Quality Report",
+    type=["xlsx"]
+)
+
+if uploaded_file:
+
+    excel_file = pd.ExcelFile(uploaded_file)
+
+    page = st.sidebar.selectbox(
+        "Select Stage",
+        excel_file.sheet_names
+    )
+
+    df = pd.read_excel(
+        uploaded_file,
+        sheet_name=page
+    )
+
+    st.header(page)
+
+    st.write("Columns Found:")
+    st.write(df.columns.tolist())
+
+    st.dataframe(
+        df.head(),
+        use_container_width=True
+    )
